2857407a9390291687aa0b5279e33b4c783561df
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-10-19 19:03:14 +0300

    serverWomazing

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // server.js
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 4000
 
+
+server.db = router.db
+
+server.use(auth)
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
